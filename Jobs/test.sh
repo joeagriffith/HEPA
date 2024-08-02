1e56caf6722b7ff98baf005af787c6879b30f613
Joe <joeagriffith@gmail.com>
2024-08-02 10:40:20 +0100

    hpc test results

--- Jobs/test.sh
+++ Jobs/test.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 #PBS -lselect=1:ncpus=4:mem=10gb:ngpus=1
-#PBS -lwalltime=00:30:00
+#PBS -lwalltime=00:10:00
 
 module load tools/prod
 module load Python/3.10.4-GCCcore-11.3.0
+
+python -c "import time;print(f'loaded modules at: {time.time()}')"
+
 source ~/ml-env/bin/activate
 
+python -c "import time;print(f'activated env at: {time.time()}')"
+
 cd ~/hepa/
+
+python -c "import time;print(f'changed directory at: {time.time()}')"
+
 python run.py test_mnist
+
+python -c "import time;print(f'finished test_mnist at: {time.time()}')"
 python run.py test_modelnet10
